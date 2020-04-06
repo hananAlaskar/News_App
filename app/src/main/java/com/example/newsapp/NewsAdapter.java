31034a3e0d6794ff84b62a982a9e63c3179eea12
HananAlaskar <hanan.f.alaskar@gmail.com>
2020-04-06 22:47:42 +0300

    change date format

--- app/src/main/java/com/example/newsapp/NewsAdapter.java
+++ app/src/main/java/com/example/newsapp/NewsAdapter.java
@@ -8,12 +8,12 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
-import android.widget.Toast;
-
 import com.example.newsapp.model.News;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
@@ -53,7 +53,7 @@ public class NewsAdapter extends ArrayAdapter<News> {
         listItemView.findViewById(R.id.news_item).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                
+
                 Uri newsUri = Uri.parse(currentNews.getmUrl());
                 Intent websiteIntent = new Intent(Intent.ACTION_VIEW, newsUri);
 
@@ -65,9 +65,36 @@ public class NewsAdapter extends ArrayAdapter<News> {
 
     private void setDataOnView(News currentNews, View listItemView) {
 
-        ((TextView)(listItemView.findViewById(R.id.news_date))).setText(currentNews.getmDate());
+
+        String date = formatDate(fromString(currentNews.getmDate()));
+        if (date != null)
+            ((TextView)(listItemView.findViewById(R.id.news_date))).setText(date);
         ((TextView)(listItemView.findViewById(R.id.news_title))).setText(currentNews.getmTitle());
         ((TextView)(listItemView.findViewById(R.id.news_type))).setText(currentNews.getmType());
 
+    }
+
+
+    private String formatDate(Date dateObject) {
+        if (dateObject == null)
+            return null;
+
+        SimpleDateFormat dateFormat = new SimpleDateFormat("LLL dd, yyyy");
+        return dateFormat.format(dateObject);
+    }
+
+    private Date fromString(String dtStart) {
+
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
+        Date date = null;
+        try {
+            date = format.parse(dtStart);
+
+            return date;
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        return date;
     }
 }
